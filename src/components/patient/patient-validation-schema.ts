691d58894a3daadf56a137e520bc1edcb44ea437
Thahir Imran <106942239+thahir-imran@users.noreply.github.com>
2025-12-27 21:07:50 +0530

    feat: update patient validation schema to include age and NIC fields with validation

--- src/components/patient/patient-validation-schema.ts
+++ src/components/patient/patient-validation-schema.ts
@@ -1,29 +1,23 @@
 import * as yup from 'yup';
-import { passwordRules } from '@/utils/constants';
 
 export const patientValidationSchema = yup.object().shape({
   first_name: yup.string().required('form:error-first-name-required'),
   last_name: yup.string().required('form:error-last-name-required'),
-  // mobile_number: yup.string().max(19, 'maximum 19 digit').optional(),
+  age: yup
+    .number()
+    .typeError('form:error-age-must-number')
+    .positive('form:error-age-must-positive')
+    .required('form:error-age-required'),
   mobile_number: yup
     .string()
     .required('form:error-contact-number-required')
     .max(19, 'maximum 19 digit'),
-  // email: yup
-  //   .string()
-  //   .email('form:error-email-format')
-  //   .required('form:error-email-required'),
+  nic: yup
+    .string()
+    .required('form:error-nic-required')
+    .matches(
+      /^(\d{9}[vV]|\d{12})$/,
+      "NIC number must be in the format 123456789V or 200012345678"
+    ),
   gender: yup.object().required('form:error-gender-required'),
-  password: yup.string().when('$isEditMode', {
-    is: true,
-    then: (schema) => schema.notRequired(),
-    otherwise: (schema) => schema.required('form:error-password-required'),
-  }),
-  // password: yup
-  //   .string()
-  //   .required('form:error-password-required')
-  //   .matches(passwordRules, {
-  //     message:
-  //       'Please create a stronger password. hint: Min 8 characters, 1 Upper case letter, 1 Lower case letter, 1 Numeric digit.',
-  //   }),
 });
