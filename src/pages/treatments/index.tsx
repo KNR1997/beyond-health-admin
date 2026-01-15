5085479de557d4fe6f74438f093c69fcfd6ef204
Thahir Imran <106942239+thahir-imran@users.noreply.github.com>
2026-01-15 07:28:44 +0530

    add function Treatments()

--- src/pages/treatments/index.tsx
+++ src/pages/treatments/index.tsx
@@ -3,13 +3,13 @@ import PageHeading from '@/components/common/page-heading';
 import Search from '@/components/common/search';
 import DentistList from '@/components/dentist/dentist-list';
 import Layout from '@/components/layouts/admin';
+import TreatmentList from '@/components/treatment/treatment-list';
 import ErrorMessage from '@/components/ui/error-message';
 import LinkButton from '@/components/ui/link-button';
 import Loader from '@/components/ui/loader/loader';
 import { Config } from '@/config';
 import { Routes } from '@/config/routes';
-import { useDentistsQuery } from '@/data/dentist';
-import { usePatientsQuery } from '@/data/patient';
+import { useTreatmentsQuery } from '@/data/treatment';
 import { SortOrder } from '@/types';
 import { adminOnly } from '@/utils/auth-utils';
 import { useTranslation } from 'next-i18next';
@@ -17,14 +17,14 @@ import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
 
-export default function Dentists() {
+export default function Treatments() {
   const { t } = useTranslation();
   const { locale } = useRouter();
   const [orderBy, setOrder] = useState('created_at');
   const [sortedBy, setColumn] = useState<SortOrder>(SortOrder.Desc);
   const [searchTerm, setSearchTerm] = useState('');
   const [page, setPage] = useState(1);
-  const { dentists, loading, paginatorInfo, error } = useDentistsQuery({
+  const { treatments, loading, paginatorInfo, error } = useTreatmentsQuery({
     language: locale,
     limit: 20,
     page,
@@ -49,7 +49,7 @@ export default function Dentists() {
     <>
       <Card className="flex flex-col items-center mb-8 md:flex-row">
         <div className="mb-4 md:mb-0 md:w-1/4">
-          <PageHeading title={t('form:input-label-dentists')} />
+          <PageHeading title={t('form:input-label-treatments')} />
         </div>
 
         <div className="flex flex-col items-center w-full space-y-4 ms-auto md:w-3/4 md:flex-row md:space-y-0 xl:w-1/2">
@@ -60,16 +60,16 @@ export default function Dentists() {
 
           {locale === Config.defaultLanguage && (
             <LinkButton
-              href={Routes.dentist.create}
+              href={Routes.treatment.create}
               className="w-full h-12 md:w-auto md:ms-6"
             >
-              <span>+ {t('form:button-label-add-dentist')}</span>
+              <span>+ {t('form:button-label-add-treatment')}</span>
             </LinkButton>
           )}
         </div>
       </Card>
-      <DentistList
-        dentists={dentists}
+      <TreatmentList
+        treatments={treatments}
         paginatorInfo={paginatorInfo}
         onPagination={handlePagination}
         onOrder={setOrder}
@@ -79,12 +79,11 @@ export default function Dentists() {
   );
 }
 
-Dentists.authenticate = {
+Treatments.authenticate = {
   permissions: adminOnly,
 };
 
-Dentists.Layout = Layout;
-
+Treatments.Layout = Layout;
 export const getStaticProps = async ({ locale }: any) => ({
   props: {
     ...(await serverSideTranslations(locale, ['form', 'common', 'table'])),
